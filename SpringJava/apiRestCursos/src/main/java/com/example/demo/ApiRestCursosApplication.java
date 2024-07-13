05f1278f441885d8d8ca44bb0908ed4a0c6a9c70
Jonathan Oblitas <oblitaskevin30@gmail.com>
2024-07-12 22:07:17 -0500

    creando el proyecto apiRestCursos

---
+++ SpringJava/apiRestCursos/src/main/java/com/example/demo/ApiRestCursosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCursosApplication.class, args);
+	}
+
+}
